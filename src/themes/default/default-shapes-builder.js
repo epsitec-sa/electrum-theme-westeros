cd3b6d9bef49ff25d614201d85857538fe6f9ebf
Daniel Roux <roux@epsitec.ch>
2016-05-09 14:47:43 +0200

    Add 'px' unit into style values. This correct a mistake difficult to find !

--- src/themes/default/default-shapes-builder.js
+++ src/themes/default/default-shapes-builder.js
@@ -3,16 +3,16 @@
 export default function (spacing) { /*jshint unused:false */
   return {
     defaultBorderRadius: 2,
-    
+
     /* geometry settings defined by Daniel Roux - to be renamed */
-    
-    lineHeight:      32,
-    containerMargin: 20,
-    lineSpacing:     10,
-    leftWidth:       80,
-    mainTabHeight:   50,
-    viewTabHeight:   32,
-    footerHeight:    70,
-    actionHeight:    50,
+
+    lineHeight:      '32px',
+    containerMargin: '20px',
+    lineSpacing:     '10px',
+    leftWidth:       '80px',
+    mainTabHeight:   '50px',
+    viewTabHeight:   '32px',
+    footerHeight:    '70px',
+    actionHeight:    '50px',
   };
 }
