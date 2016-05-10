fa71c2a093f219f34bbfd8e8389e8aba38ff6c9c
Daniel Roux <roux@epsitec.ch>
2016-05-10 11:45:57 +0200

    Move and rename unit-helpers.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 import ColorManipulator from './themes/color-manipulator.js';
+import Unit from './unit.js';
 
 export {Theme} from './theme.js';
 export {Styles} from './styles.js';
 
 export {ColorManipulator};
+export {Unit};
