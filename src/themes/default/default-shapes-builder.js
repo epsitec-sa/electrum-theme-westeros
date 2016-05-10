5b85692fcf94a8221538a9b34f02c2a83c939f7b
Daniel Roux <roux@epsitec.ch>
2016-05-10 08:37:19 +0200

    Added new geometry themes (in mock style).

--- src/themes/default/default-shapes-builder.js
+++ src/themes/default/default-shapes-builder.js
@@ -6,8 +6,19 @@ export default function (spacing) { /*jshint unused:false */
 
     /* geometry settings defined by Daniel Roux - to be renamed */
 
+    // Géométrie définie par Gilles:
     lineHeight:      '32px',
     containerMargin: '20px',
     lineSpacing:     '10px',
+
+    // Géométrie compacte:
+    // lineHeight:      '25px',
+    // containerMargin: '10px',
+    // lineSpacing:     '5px',
+
+    // Grands champs serrés:
+    // lineHeight:      '30px',
+    // containerMargin: '10px',
+    // lineSpacing:     '-1px',
   };
 }
