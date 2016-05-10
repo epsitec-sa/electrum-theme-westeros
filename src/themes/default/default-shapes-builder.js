249f0412767c432534218dfb4bad744ad2f034a5
Daniel Roux <roux@epsitec.ch>
2016-05-10 13:26:37 +0200

    Added many theme.shapes.

--- src/themes/default/default-shapes-builder.js
+++ src/themes/default/default-shapes-builder.js
@@ -8,12 +8,31 @@ export default function (spacing) { /*jshint unused:false */
 
     /* geometry settings defined by Daniel Roux - to be renamed */
 
-    lineHeight:      spacing.lineHeight,
-    containerMargin: spacing.containerMargin,
-    lineSpacing:     spacing.lineSpacing,
+    lineHeight:            spacing.lineHeight,
+    containerMargin:       spacing.containerMargin,
+    lineSpacing:           spacing.lineSpacing,
 
-    taskButtonWidth:  Unit.multiply (spacing.lineHeight, 2.5),
-    taskButtonHeight: Unit.multiply (spacing.lineHeight, 3.0),
+    taskButtonWidth:       Unit.multiply (spacing.lineHeight, 2.5),
+    taskButtonHeight:      Unit.multiply (spacing.lineHeight, 3.0),
+    taskLogoTextSize:      '125%',
+    taskTextSize:          '80%',
+    taskShadow:            '0px 0px 60px rgba(0, 0, 0, 0.50)',
+
+    mainTabHeight:         Unit.multiply (spacing.lineHeight, 1.5),
+    mainTabTextSize:       '125%',
+
+    viewTabHeight:         Unit.multiply (spacing.lineHeight, 1.0),
+    viewTabTextSize:       '80%',
+
+    paneNavigatorHeight:   Unit.multiply (spacing.lineHeight, 1.0),
+    paneNavigatorTextSize: '100%',
+
+    footerHeight:          Unit.multiply (spacing.lineHeight, 1.5),
+    footerTextSize:        '100%',
+
+    actionHeight:          Unit.multiply (spacing.lineHeight, 1.5),
+    actionRadius:          Unit.multiply (spacing.lineHeight, 0.75),
+    actionTextSize:        '100%',
 
   };
 }
