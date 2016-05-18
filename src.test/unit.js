44e1ac9bea156663addc249b5085acacc4831a4a
Daniel Roux <roux@epsitec.ch>
2016-05-18 13:04:42 +0200

    Complete src.test/unit.js, without testing (Wallaby don't run on my computer).

--- src.test/unit.js
+++ src.test/unit.js
@@ -23,11 +23,37 @@ describe ('Unit', () => {
   });
 
   describe ('add()', () => {
-    // TODO: test add()
+    it ('add known units', () => {
+      expect (Unit.add (2, 3)).to.deep.equal ({value: 5, unit: 'px'});
+      expect (Unit.add (2.2, 3.3)).to.deep.equal ({value: 5.5, unit: 'px'});
+      expect (Unit.add ('2px', '3px')).to.deep.equal ({value: 5, unit: 'px'});
+      expect (Unit.add ('2.2px', '3.3px')).to.deep.equal ({value: 5.5, unit: 'px'});
+      expect (Unit.add ('2.2rem', '3.3rem')).to.deep.equal ({value: 5.5, unit: 'rem'});
+      expect (Unit.add ('2.2em', '3.3em')).to.deep.equal ({value: 5.5, unit: 'em'});
+      expect (Unit.add ('75%', '5%')).to.deep.equal ({value: 80, unit: '%'});
+    });
+    it ('throws on unexpected input', () => {
+      expect (() => Unit.add ('2pt', '3pt')).to.throw ('5pt');
+      expect (() => Unit.add ('2px', 3)).to.throw ('5px');
+      expect (() => Unit.add ('5px', '10%')).to.throw ('5.5px');
+    });
   });
 
   describe ('sub()', () => {
-    // TODO: test sub()
+    it ('add known units', () => {
+      expect (Unit.sub (3, 2)).to.deep.equal ({value: 1, unit: 'px'});
+      expect (Unit.sub (3.3, 2.2)).to.deep.equal ({value: 1.1, unit: 'px'});
+      expect (Unit.sub ('3px', '2px')).to.deep.equal ({value: 1, unit: 'px'});
+      expect (Unit.sub ('3.3px', '2.2px')).to.deep.equal ({value: 1.1, unit: 'px'});
+      expect (Unit.sub ('3.3rem', '2.2rem')).to.deep.equal ({value: 1.1, unit: 'rem'});
+      expect (Unit.sub ('3.3em', '2.2em')).to.deep.equal ({value: 1.1, unit: 'em'});
+      expect (Unit.sub ('75%', '5%')).to.deep.equal ({value: 70, unit: '%'});
+    });
+    it ('throws on unexpected input', () => {
+      expect (() => Unit.sub ('2pt', '3pt')).to.throw ('5pt');
+      expect (() => Unit.sub ('2px', 3)).to.throw ('5px');
+      expect (() => Unit.sub ('5px', '10%')).to.throw ('5.5px');
+    });
   });
 
   describe ('parse()', () => {
@@ -38,6 +64,7 @@ describe ('Unit', () => {
       expect (Unit.parse ('5.1px')).to.deep.equal ({value: 5.1, unit: 'px'});
       expect (Unit.parse ('5rem')).to.deep.equal ({value: 5, unit: 'rem'});
       expect (Unit.parse ('5em')).to.deep.equal ({value: 5, unit: 'em'});
+      expect (Unit.parse ('75%')).to.deep.equal ({value: 75, unit: '%'});
     });
     it ('throws on unexpected input', () => {
       expect (() => Unit.parse ('5pt')).to.throw ('5pt');
