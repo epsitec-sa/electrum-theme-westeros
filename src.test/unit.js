4c4519f7fe8ded27962574a64694f5718d6176a5
Daniel Roux <roux@epsitec.ch>
2016-05-18 16:22:44 +0200

    dd some tests with negative numbers.

--- src.test/unit.js
+++ src.test/unit.js
@@ -20,26 +20,38 @@ describe ('Unit', () => {
   });
 
   describe ('fix()', () => {
-    it ('fix without decimal', () => {
+    it ('fix without decimal numbers', () => {
       expect (Unit.fix (5, null)).to.equal (5);
-      expect (Unit.fix (5, 0)).to.equal (5);
+      expect (Unit.fix (5, 0)).to.equal ('5');
       expect (Unit.fix (5, 1)).to.equal ('5.0');
       expect (Unit.fix (5, 2)).to.equal ('5.00');
       expect (Unit.fix (5, 3)).to.equal ('5.000');
     });
-    it ('fix with .1', () => {
+    it ('fix with .1 numbers', () => {
       expect (Unit.fix (5.1, null)).to.equal (5.1);
-      expect (Unit.fix (5.1, 0)).to.equal (5.1);
+      expect (Unit.fix (5.1, 0)).to.equal ('5');
       expect (Unit.fix (5.1, 1)).to.equal ('5.1');
       expect (Unit.fix (5.1, 2)).to.equal ('5.10');
       expect (Unit.fix (5.1, 3)).to.equal ('5.100');
     });
-    it ('fix with .9', () => {
+    it ('fix with .9 numbers', () => {
       expect (Unit.fix (5.9, null)).to.equal (5.9);
-      expect (Unit.fix (5.9, 0)).to.equal (5.9);
+      expect (Unit.fix (5.9, 0)).to.equal ('6');
       expect (Unit.fix (5.9, 1)).to.equal ('5.9');
       expect (Unit.fix (5.9, 2)).to.equal ('5.90');
       expect (Unit.fix (5.9, 3)).to.equal ('5.900');
+    });
+    it ('fix with negative numbers', () => {
+      expect (Unit.fix (-5.1, null)).to.equal (-5.1);
+      expect (Unit.fix (-5.1, 0)).to.equal ('-5');
+      expect (Unit.fix (-5.1, 1)).to.equal ('-5.1');
+      expect (Unit.fix (-5.1, 2)).to.equal ('-5.10');
+      expect (Unit.fix (-5.1, 3)).to.equal ('-5.100');
+      expect (Unit.fix (-5.9, null)).to.equal (-5.9);
+      expect (Unit.fix (-5.9, 0)).to.equal ('-6');
+      expect (Unit.fix (-5.9, 1)).to.equal ('-5.9');
+      expect (Unit.fix (-5.9, 2)).to.equal ('-5.90');
+      expect (Unit.fix (-5.9, 3)).to.equal ('-5.900');
     });
   });
 
@@ -49,19 +61,34 @@ describe ('Unit', () => {
       expect (Unit.add (2.2, 3.3)).to.deep.equal (5.5);
       expect (Unit.add ('2px', '3px')).to.deep.equal ('5px');
       expect (Unit.add ('2.0px', '3.00px')).to.deep.equal ('5px');
+      expect (Unit.add ('2px', '3px', 0)).to.deep.equal ('5px');
       expect (Unit.add ('2px', '3px', 2)).to.deep.equal ('5.00px');
       expect (Unit.add ('2px', '3px', 5)).to.deep.equal ('5.00000px');
       expect (Unit.add ('2.2px', '3.3px')).to.deep.equal ('5.5px');
       expect (Unit.add ('2.4px', '3.5px')).to.deep.equal ('5.9px');
       expect (Unit.add ('2.4px', '3.6px')).to.deep.equal ('6px');
+      expect (Unit.add ('2.4px', '3.5px', 0)).to.deep.equal ('6px');
+      expect (Unit.add ('2.4px', '3.5px', 1)).to.deep.equal ('5.9px');
       expect (Unit.add ('2.4px', '3.5px', 2)).to.deep.equal ('5.90px');
       expect (Unit.add ('2.4px', '3.6px', 2)).to.deep.equal ('6.00px');
+      expect (Unit.add ('2.2222px', '3.1px', 0)).to.deep.equal ('5px');
       expect (Unit.add ('2.2222px', '3.1px', 2)).to.deep.equal ('5.32px');
+      expect (Unit.add ('2.9999px', '3.1px', 0)).to.deep.equal ('6px');
       expect (Unit.add ('2.9999px', '3.1px', 2)).to.deep.equal ('6.10px');
+      expect (Unit.add ('2.9px', '3.1px', 0)).to.deep.equal ('6px');
       expect (Unit.add ('2.9px', '3.1px', 2)).to.deep.equal ('6.00px');
       expect (Unit.add ('2.2rem', '3.3rem')).to.deep.equal ('5.5rem');
       expect (Unit.add ('2.2em', '3.3em')).to.deep.equal ('5.5em');
       expect (Unit.add ('75%', '5%', null)).to.deep.equal ('80%');
+    });
+    it ('add negative numbers', () => {
+      expect (Unit.add (-2, 3)).to.deep.equal (1);
+      expect (Unit.add (2, -3)).to.deep.equal (-1);
+      expect (Unit.add (-2, -3)).to.deep.equal (-5);
+      expect (Unit.add (2.2, -3.3)).to.deep.equal (-1.0999999999999996);
+      expect (Unit.add ('-2px', '3px')).to.deep.equal ('1px');
+      expect (Unit.add ('2px', '-3px')).to.deep.equal ('-1px');
+      expect (Unit.add ('-2px', '-3px')).to.deep.equal ('-5px');
     });
     it ('throws on unexpected input', () => {
       expect (() => Unit.add ('2pt', '3pt')).to.throw ();
@@ -73,23 +100,39 @@ describe ('Unit', () => {
   describe ('sub()', () => {
     it ('add known units', () => {
       expect (Unit.sub (3, 2)).to.deep.equal (1);
-      expect (Unit.sub (3, 2, null)).to.deep.equal (1);
+      expect (Unit.sub (3.3, 2.2, null)).to.deep.equal (1.0999999999999996);
       expect (Unit.sub ('3px', '2px')).to.deep.equal ('1px');
       expect (Unit.sub ('3.0px', '2.00px')).to.deep.equal ('1px');
       expect (Unit.sub ('3px', '2px', null)).to.deep.equal ('1px');
+      expect (Unit.sub ('3px', '2px', 0)).to.deep.equal ('1px');
       expect (Unit.sub ('3px', '2px', 2)).to.deep.equal ('1.00px');
       expect (Unit.sub ('3px', '2px', 5)).to.deep.equal ('1.00000px');
       expect (Unit.sub ('3.3px', '2.2px')).to.deep.equal ('1.0999999999999996px');
+      expect (Unit.sub ('3.3px', '2.2px', 0)).to.deep.equal ('1px');
+      expect (Unit.sub ('3.3px', '2.2px', 1)).to.deep.equal ('1.1px');
       expect (Unit.sub ('3.3px', '2.2px', 2)).to.deep.equal ('1.10px');
       expect (Unit.sub ('3.9px', '2.1px')).to.deep.equal ('1.7999999999999998px');
+      expect (Unit.sub ('3.9px', '2.1px', 0)).to.deep.equal ('2px');
       expect (Unit.sub ('3.9px', '2.1px', 1)).to.deep.equal ('1.8px');
       expect (Unit.sub ('3.9px', '2.1px', 2)).to.deep.equal ('1.80px');
+      expect (Unit.sub ('3.2222px', '2.1px', 0)).to.deep.equal ('1px');
       expect (Unit.sub ('3.2222px', '2.1px', 2)).to.deep.equal ('1.12px');
+      expect (Unit.sub ('3.9999px', '2.1px', 0)).to.deep.equal ('2px');
       expect (Unit.sub ('3.9999px', '2.1px', 2)).to.deep.equal ('1.90px');
+      expect (Unit.sub ('3.9px', '2.9px', 0)).to.deep.equal ('1px');
       expect (Unit.sub ('3.9px', '2.9px', 2)).to.deep.equal ('1.00px');
       expect (Unit.sub ('3rem', '2rem')).to.deep.equal ('1rem');
       expect (Unit.sub ('3em', '2em')).to.deep.equal ('1em');
       expect (Unit.sub ('75%', '5%')).to.deep.equal ('70%');
+    });
+    it ('sub negative numbers', () => {
+      expect (Unit.sub (-2, 3)).to.deep.equal (-5);
+      expect (Unit.sub (2, -3)).to.deep.equal (5);
+      expect (Unit.sub (-2, -3)).to.deep.equal (1);
+      expect (Unit.sub (2.2, -3.3)).to.deep.equal (5.5);
+      expect (Unit.sub ('-2px', '3px')).to.deep.equal ('-5px');
+      expect (Unit.sub ('2px', '-3px')).to.deep.equal ('5px');
+      expect (Unit.sub ('-2px', '-3px')).to.deep.equal ('1px');
     });
     it ('throws on unexpected input', () => {
       expect (() => Unit.sub ('2pt', '3pt')).to.throw ();
