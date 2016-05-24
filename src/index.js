90c4b0dea4e9bd4ce495264ea6694856f43923a1
Pierre Arnaud <arnaud@epsitec.ch>
2016-05-24 10:31:52 +0200

    Export ThemeConfigurations.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 'use strict';
 
 import ColorManipulator from './themes/color-manipulator.js';
-import findTheme from './find-theme.js';
 import Unit from './themes/unit.js';
+import findTheme from './find-theme.js';
+import ThemeConfigurations from './theme-configurations.js';
 
 export {Theme} from './theme.js';
 export {Styles} from './styles.js';
 
 export {ColorManipulator};
 export {findTheme};
+export {ThemeConfigurations};
 export {Unit};
