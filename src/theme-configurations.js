f6c12625f1d8122685947524b7e9706f4d45c7ea
Daniel Roux <roux@epsitec.ch>
2016-07-07 11:50:59 +0200

    Remove unused dark theme (fix a run error).

--- src/theme-configurations.js
+++ src/theme-configurations.js
@@ -2,7 +2,6 @@
 
 import CompactMono from './theme-configurations/compact-mono.js';
 import CompactPink from './theme-configurations/compact-pink.js';
-import Dark from './theme-configurations/dark.js';
 import Default from './theme-configurations/default.js';
 import DefaultGreen from './theme-configurations/default-green.js';
 import DefaultPink from './theme-configurations/default-pink.js';
@@ -13,7 +12,6 @@ import SmoothGreen from './theme-configurations/smooth-green.js';
 
 export default {
   CompactMono, CompactPink,
-  Dark,
   Default, DefaultGreen, DefaultPink,
   SpecialGreen, SmoothGreen,
 };
