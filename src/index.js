a465b2cb6acde0cb36840e9b0c864a457fd9c107
Daniel Roux <roux@epsitec.ch>
2016-08-17 20:11:34 +0200

    Rename mark-color to color-helpers.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import ColorManipulator from './themes/color-manipulator.js';
-import MarkColor from './themes/mark-color.js';
+import ColorHelpers from './themes/color-helpers.js';
 import Unit from './themes/unit.js';
 import findTheme from './find-theme.js';
 import ThemeConfigurations from './theme-configurations.js';
@@ -10,7 +10,7 @@ export {Theme} from './theme.js';
 export {Styles} from './styles.js';
 
 export {ColorManipulator};
-export {MarkColor};
+export {ColorHelpers};
 export {findTheme};
 export {ThemeConfigurations};
 export {Unit};
--- src/themes/color-helpers.js
+++ src/themes/color-helpers.js
@@ -4,19 +4,19 @@
 
 // Return the color to use from a value of a property.
 // You can use a direct color '#rrggbb' or the key word 'primary' or 'secondary'.
-// In this cases, the color is defined into theme.palette.
-function GetColor (theme, mark) {
-  if (mark.startsWith ('#')) {
-    return mark;
+// In this two cases, the color is defined into theme.palette.mark*.
+function GetMarkColor (theme, value) {
+  if (value.startsWith ('#')) {
+    return value;
   } else {
     const fix = {
       primary:   theme.palette.markPrimary,
       secondary: theme.palette.markSecondary
     };
-    return fix[mark];
+    return fix[value];
   }
 }
 
 module.exports = {
-  GetColor,
+  GetMarkColor,
 };
