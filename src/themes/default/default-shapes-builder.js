4a7d6e7bb1bf24239372a3c3b262e7983494c22d
Daniel Roux <roux@epsitec.ch>
2016-09-06 21:12:47 +0200

    Put values to themes.

--- src/themes/default/default-shapes-builder.js
+++ src/themes/default/default-shapes-builder.js
@@ -104,5 +104,7 @@ export default function (spacing) { /*jshint unused:false */
     dialogShadow:              '0px 20px 100px rgba(0, 0, 0, 0.8)',
 
     loginShadow:               '0px 10px 100px rgba(0, 0, 0, 0.80)',
+    loginPadding:              Unit.multiply (spacing.containerMargin, 2),
+    loginRadius:               Unit.multiply (spacing.containerMargin, 0.5),
   };
 }
