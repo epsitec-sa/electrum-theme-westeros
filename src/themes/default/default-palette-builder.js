e50c0938dd9c8ae5862aeec059cb9db2f7e74cd3
Daniel Roux <roux@epsitec.ch>
2016-11-23 19:58:03 +0100

    Show a warning if a pick is under a drop, or reverse.

--- src/themes/default/default-palette-builder.js
+++ src/themes/default/default-palette-builder.js
@@ -142,6 +142,7 @@ export default function (colors) {
     ticketGlueTitle:                    emphasize (colors.dark, 1.0),
     ticketHatchOpacity:                 0.1,
     ticketDimmed:                       emphasize (colors.light, 0.3),
+    ticketWarningBackground:            colors.warning,
 
     warningBackground:                  colors.warning,
     warningText:                        colors.dark,
