f7ee3987ef0b57de370dd83f6d620ba220d9f376
Daniel Roux <roux@epsitec.ch>
2017-02-15 10:58:17 +0100

    WIP: Replace horizontal scroller by d&d scrolling (same strategy with vertical scrolling).

--- src/themes/default/default-shapes-builder.js
+++ src/themes/default/default-shapes-builder.js
@@ -83,7 +83,7 @@ export default function (spacing) { /*jshint unused:false */
     eventPadding:                  spacing.lineSpacing,
     chronosTopHeight:              '48px',
     chronosLineHeight:             '32px',
-    chronosSeparatorHeight:        '16px',
+    chronosSeparatorHeight:        '32px',
 
     markWidth:                     Unit.multiply (spacing.containerMargin, 0.3),
 
